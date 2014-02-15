d1b09edd4fe45b77b5b532b70a18d7e68bc6dc87
khalidmnet <khalidmnet@192.168.1.185>
2014-02-15 18:35:48 +0300

    Added Slider

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,13 +1,24 @@
-$(document).ready(function(){
-    
+$(document).ready(function() {
+
+    //slider
+    $('.slider').unslider({
+        speed: 500, //  The speed to animate each slide (in milliseconds)
+        delay: 3000, //  The delay between slide animations (in milliseconds)
+        complete: function() {
+        }, //  A function that gets called after every slide animation
+        keys: true, //  Enable keyboard (left, right) arrow shortcuts
+        dots: true, //  Display dot navigation
+        fluid: true              //  Support responsive design. May break non-responsive designs
+    });
+
     //mobile nav-menu
     var navActive = false;
-    $("#navLink").click(function(){
-        if(!navActive){
+    $("#navLink").click(function() {
+        if (!navActive) {
             $('nav').slideDown();
             $(this).addClass('active');
             navActive = true;
-        }else{
+        } else {
             $('nav').slideUp();
             $(this).removeClass('active');
             navActive = false;
