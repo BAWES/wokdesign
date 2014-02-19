0ef095e1e7e2b7d242d75a0fce5053d7c40c51d8
khalidmnet <khalid@khalidm.net>
2014-02-19 17:37:09 +0300

    Added Ingredient Carousel

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -74,4 +74,57 @@ $(document).ready(function() {
        $('header#box a.fbShare').show();
        $('header#box a.twitterShare').show();
     });
+    
+    //owl
+    $("#ingredientList").owlCarousel({
+        
+    // Most important owl features
+    items : 4,
+    itemsCustom : false,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,2],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : false,
+    itemsScaleUp : false,
+ 
+    //Basic Speeds
+    slideSpeed : 200,
+    paginationSpeed : 800,
+    rewindSpeed : 1000,
+ 
+    //Autoplay
+    autoPlay : false,
+    stopOnHover : false,
+ 
+    // Navigation
+    navigation : false,
+    navigationText : ["prev","next"],
+    rewindNav : true,
+    scrollPerPage : false,
+ 
+    //Pagination
+    pagination : true,
+    paginationNumbers: false,
+ 
+    // Responsive 
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,
+ 
+    // CSS Styles
+    baseClass : "owl-carousel",
+    theme : "owl-theme",
+ 
+    //Mouse Events
+    dragBeforeAnimFinish : true,
+    mouseDrag : true,
+    touchDrag : true,
+ 
+    // Other
+    addClassActive : false,
+ 
+ 
+    });
 });
